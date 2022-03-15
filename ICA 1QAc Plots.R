e750c68868dd66f2a251102f4d10f4dff99ed842
Samin-K <95353496+Samin-K@users.noreply.github.com>
2022-03-15 19:10:44 +0000

    Add files via upload

--- ICA 1QAc Plots.R
+++ ICA 1QAc Plots.R
@@ -1,12 +1,16 @@
+rm(list=ls(all=TRUE))
+
+dev.off()
 lambda <- 2
 bet <- 2
 alph <- 3
 
+###PDF
 
 dens <- function(z,n) {partialsum = 0
 
 for(k in 1:n){
-partialsum = partialsum + (lambda^k * exp(-lambda)/factorial(k)) * bet^(alph*k)*z*(alph * k -1) * exp(-bet*z)/gamma(alph*k)}
+  partialsum = partialsum + (lambda^k * exp(-lambda)/factorial(k)) * bet^(alph*k)*z*(alph * k -1) * exp(-bet*z)/gamma(alph*k)}
 
 return(partialsum)
 }
@@ -18,20 +22,18 @@ plot(zs1,dens(zs1,200),"l")
 
 
 
-
-
-
+####CDF
 
 gammadist <- function(u,i) {bet^(alph*i)*u*(alph * i -1) * exp(-bet*u)/gamma(alph*i)}
 
 
-cdf <- function(z,n) {partialsum = exp(-lambda)
-  if(n>0){
-
+cdf <- function(z,n) {partialsum = lambda * exp(-lambda)
+if(n>0){
+  
   for(k in 1:n){
     partialsum = partialsum + (lambda^k * exp(-lambda)/factorial(k)) * integrate(gammadist,0,z,i=k)$value
   } 
-  } 
+} 
 
 return(partialsum)
 }
@@ -45,4 +47,38 @@ for(i in 1:length(zs2)){
 
 
 plot(zs2,cdf_vals_list,"l")
+
+#Monte Carlo Simulation_______________________________________________________________
+
+poisson_gamma_montecarlo <- function(lambda,alpha,beta,nsim){
+  
+  annual_loss <- rep(NA,nsim)
+  
+  for (i in 1:nsim){
+    
+    no_of_losses <- rpois(1,lambda)
+    
+    if (no_of_losses == 0){
+      
+      annual_loss[i] <- 0
+      
+    } else{
+      
+      individual_losses = rgamma(no_of_losses,alpha, beta)
+      
+      agg_losses = sum(individual_losses)
+      annual_loss[i] <- agg_losses
+      
+    }
+    
+  }
+
+  
+  return(annual_loss)
+
+}
+
+monty = poisson_gamma_montecarlo(lambda = lambda,alpha = alph,beta = bet, nsim = 10000)
+
+hist(monty)
 
--- ICA 1QBc Plots.R
+++ ICA 1QBc Plots.R
@@ -1,78 +1,95 @@
-lambda <- 2
-gamm <- 3
-delt <- 3
+rm(list=ls(all=TRUE))
+dev.off()
+
+library(pracma)
+
+library(rmutil)
+lambda <- 10
+gamm <- 0.01
+delt <- 0
 
 bet_tilde <- 1
 
-gamm_tilde_half = function(n) {n * abs(gamm)^0.5}
+gamm_tilde_power = function(n) {n * abs(gamm)^0.5}
 
-delt_tilde = function(n) {n*delt + tan(pi/4) * (n^2 *abs(gamm) - n*gamm)}
+delt_tilde = function(n) {n*delt + (n^2 *abs(gamm) - n*gamm)}
 
 
-dens <- function(z,n) {partialsum = 0
-  for(k in 1:n){
-    if(delt_tilde(k)<z){
-      partialsum = partialsum + (lambda^k * exp(-lambda)/factorial(k)) *  gamm_tilde_half(k)/(2*pi) * (z-delt_tilde(k))^(-1.5) * exp(-gamm_tilde_half(k)/(2*(z-delt_tilde(k))))}
+dens <- function(z,n) {partialsum = 0 
+for(k in 1:n){
+  if(delt_tilde(k)<z){
+    partialsum = partialsum + (lambda^k * exp(-lambda)/factorial(k)) *  gamm_tilde_power(k)/sqrt(2*pi) * (z-delt_tilde(k))^(-3/2) * exp(-gamm_tilde_power(k)^2/(2*(z-delt_tilde(k))))}
 }
-  return(partialsum)
-}
-
-zs1 = seq(0, 50, by = 0.01)
-dens_vals_list <- c(length(zs1))
-
-for(i in 1:length(zs1)){
-  dens_vals_list[i] <- dens(zs1[i],20)}
-
-plot(zs1,dens_vals_list,"l")
-
-#integrate(dens,0,1000,n=50)
-
-cdf <- function(z,n) {partialsum = exp(-lambda) 
-  for(k in 1:n){
-    if(delt_tilde(k)<z){
-      partialsum = partialsum + (lambda^k * exp(-lambda)/factorial(k)) *  erfc(gamm_tilde_half(k)/sqrt((2*(z-delt_tilde(k)))))
-  }
-  return(partialsum)
-  }
-  }
-
-zs2 = seq(0, 500, by = 0.1)
-cdf_vals_list <- c(1:5001)
-length(zs2)
-length(cdf_vals_list)
-
-
-for(i in 1:length(zs2)){
-  cdf_vals_list[i] <- cdf(zs1[i],100)}
-
-length(zs2)
-length(cdf_vals_list)
-plot(zs2,cdf_vals_list,"l")
-
-
-
-
-
-gammadist <- function(u,i) {bet^(alph*i)*u*(alph * i -1) * exp(-bet*u)/gamma(alph*i)}
-
-
-cdf <- function(z,n) {partialsum = exp(-lambda)
-if(n>0){
-  
-  for(k in 1:n){
-    partialsum = partialsum + (lambda^k * exp(-lambda)/factorial(k)) * integrate(gammadist,0,z,i=k)$value
-  } 
-} 
-
 return(partialsum)
 }
 
 
-zs2 = seq(0, 10, by = 0.01)
-cdf_vals_list <- c(length(zs2))
+zs1 = seq(0, 49, by = 1)
+dens_vals_list <- c(length(zs1))
+
+for(i in 1:length(zs1)){
+  dens_vals_list[i] <- dens(zs1[i],2000)}
+
+plot(zs1,dens_vals_list,"l")
+
+#________________________________________________________________________________
+
+
+
+cdf <- function(z,n) {partialsum = exp(-lambda) 
+for(k in 1:n){
+  if(delt_tilde(k)<z){
+    partialsum = partialsum + (lambda^k * exp(-lambda)/factorial(k)) *  erfc(gamm_tilde_power(k)/sqrt((2*(z-delt_tilde(k)))))
+  }
+  return(partialsum)
+}
+}
+
+
+zs2 = seq(0, 49, by = 1)
+cdf_vals_list <- vector(mode = "numeric",length= length(zs2))
+
+length(zs2)
+length(cdf_vals_list)
 
 for(i in 1:length(zs2)){
-  cdf_vals_list[i] <- cdf(zs2[i],30)}
+  cdf_vals_list[i] <- cdf(zs2[i],100)}
+
+length(zs2)
+length(cdf_vals_list)
+plot(zs2,cdf_vals_list,ylim=c(0,0.02),"l")
 
 
-plot(zs2,cdf_vals_list,"l")
+#Monte Carlo Simulation_______________________________________________________________
+
+poisson_levy_montecarlo <- function(lambda,gamma,delta,nsim){
+  
+  annual_loss <- rep(NA,nsim)
+  
+  for (i in 1:nsim){
+    
+    no_of_losses <- rpois(1,lambda)
+    
+    if (no_of_losses == 0){
+      
+      annual_loss[i] <- 0
+      
+    } else{
+      
+      individual_losses = rlevy(no_of_losses,gamma, delta)
+      
+      agg_losses = sum(individual_losses)
+      annual_loss[i] <- agg_losses
+      
+    }
+    
+  }
+  
+  
+  return(annual_loss)
+  
+}
+levy = poisson_levy_montecarlo(lambda = lambda,gamma = gamm, delta = delt, nsim = 10000)
+
+hist(levy)
+
